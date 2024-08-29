a48d708ef1b52d0311ad15cb9ee3691b887cda0d
ahhchu <catchuuu2103@gmail.com>
2024-08-29 16:14:51 -0400

    Initial Commit

---
+++ GKnow/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GKnow
+//
+//  Created by Catherine Chu on 8/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GKnow/GKnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GKnowApp.swift
+//  GKnow
+//
+//  Created by Catherine Chu on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GKnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
